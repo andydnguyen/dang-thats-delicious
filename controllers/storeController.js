5258508c3d696b1b565874b8e78075ee87fc6f0c
Andy Nguyen <anguyen1119@gmail.com>
2019-09-15 21:08:57 -0400

    Add Store form

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,8 +1,12 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Wes';
-  next();
-};
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore= (req, res) => {
+  res.json(req.body);
 };
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const storeController = require('../controllers/storeController.js');
 
 // Do work here
-router.get('/', storeController.myMiddleware, storeController.homePage);
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 module.exports = router;
