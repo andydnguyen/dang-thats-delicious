409cb835324b930a45760636311192d6602b9440
Andy Nguyen <anguyen1119@gmail.com>
2019-09-22 22:12:27 -0400

    Add tags page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -89,3 +89,12 @@ exports.getStoreBySlug = async (req, res, next) => {
   if (!store) return next();
   res.render('store', { store, title: store.name });
 };
+
+exports.getStoresByTag = async (req, res) => {
+  const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true };
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+  res.render('tag', { tags, title: 'Tags', tag, stores });
+};
