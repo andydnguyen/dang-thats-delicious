f9c25ef7b230473ab212c50d63b6c2ae75dcf804
Andy Nguyen <anguyen1119@gmail.com>
2019-10-03 22:13:37 -0400

    Add user accounts

--- start.js
+++ start.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 // Make sure we are running node 7.6+
 const [major, minor] = process.versions.node.split('.').map(parseFloat);
 if (major < 7 || (major === 7 && minor <= 5)) {
-  console.log('🛑 🌮 🐶 💪 💩\nHey You! \n\t ya you! \n\t\tBuster! \n\tYou\'re on an older version of node that doesn\'t support the latest and greatest things we are learning (Async + Await)! Please go to nodejs.org and download version 7.6 or greater. 👌\n ');
+  console.log(
+    "🛑 🌮 🐶 💪 💩\nHey You! \n\t ya you! \n\t\tBuster! \n\tYou're on an older version of node that doesn't support the latest and greatest things we are learning (Async + Await)! Please go to nodejs.org and download version 7.6 or greater. 👌\n "
+  );
   process.exit();
 }
 
@@ -13,7 +15,7 @@ require('dotenv').config({ path: 'variables.env' });
 // Connect to our Database and handle any bad connections
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
-mongoose.connection.on('error', (err) => {
+mongoose.connection.on('error', err => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
@@ -21,6 +23,7 @@ mongoose.connection.on('error', (err) => {
 
 // import all of our models
 require('./models/Store');
+require('./models/User');
 
 // Start our app!
 const app = require('./app');
