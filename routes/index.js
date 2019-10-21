7182569c3e94d2b5ecea1b3dce9dcbb26d9381f5
Andy Nguyen <anguyen1119@gmail.com>
2019-10-20 21:35:04 -0400

    Add store locator tab

--- routes/index.js
+++ routes/index.js
@@ -55,11 +55,13 @@ router.post(
   authController.confirmedPasswords,
   catchErrors(authController.update)
 );
+router.get('/map', storeController.mapPage);
 
 /*
   API
 */
 
 router.get('/api/search', catchErrors(storeController.searchStores));
+router.get('/api/stores/near', catchErrors(storeController.mapStores));
 
 module.exports = router;
